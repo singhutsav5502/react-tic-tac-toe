6a351a5ff38c4b6490e22a609dfbe82cbb1975d0
singhutsav5502 <singhutsavofficial@gmail.com>
2023-01-21 20:55:27 +0530

    handle Click added

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ class Square extends React.Component {
     handleClick(i){
         const squares = this.state.squares.slice();
         squares[i] = 'X';
-        this.setState({squares:squares});
+        this.setState({squares:squares});   
     }
     constructor(props){
         super(props);
