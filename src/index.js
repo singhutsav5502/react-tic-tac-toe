3011095e912ff26a03fac0768f95f05a0a415d08
singhutsav5502 <singhutsavofficial@gmail.com>
2023-01-21 12:30:24 +0530

    shoppingList

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+
+class ShoppingList extends React.Component{
+    render() {
+         return (
+            <div className="shopping-List">
+                <h1>Shopping List for {this.props.name}</h1>
+                <ul>
+                    <li>Instagram</li>
+                    <li>WhatsApp</li>
+                    <li>Oculus</li>
+                </ul>
+            </div>
+         );
+    }
+}
+// Example Usage : <ShoppingList name = "Check" />
+ 
