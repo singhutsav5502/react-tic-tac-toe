b4afefca8887d335e0652ecc1b0563f7446a6fa4
singhutsav5502 <singhutsavofficial@gmail.com>
2023-01-22 17:01:42 +0530

    used function declaration

--- src/index.js
+++ src/index.js
@@ -19,9 +19,9 @@ function Square(props){
   return (
     <button 
       className = "square"
-      onClick = {()=> this.props.onClick()}
+      onClick = {()=> props.onClick()}
       >
-        {this.props.value}
+        {props.value}
       </button>
   )
 }
